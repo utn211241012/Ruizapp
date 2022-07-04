01a9976bb12e778d69c305446a2742aa2542f725
utn211241012 <101811558+utn211241012@users.noreply.github.com>
2022-07-03 22:10:16 -0500

    Update HomeController.java

--- src/main/java/com/Ferreira/Huerta/Luis/Angel/org/Controller/HomeController.java
+++ src/main/java/com/Ferreira/Huerta/Luis/Angel/org/Controller/HomeController.java
@@ -6,8 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	@Autowired
+	private IntVacantes VacantesServceImp;
+	@Autowired
+	private IntCategorias CategoriasServceImp;
+	
 	@GetMapping("/")
-	public  String mostrarHome() {
+	public String mostrarHome(Model model) {
+		List<Vacante> lista = VacantesServceImp.obtenerTodas();
+		List<Categoria> catego = CategoriasServceImp.ObtenerTodas();
+		model.addAttribute("vacantes", lista);
+		model.addAttribute("categorias", catego);
 		return "home";
 	}
 }
