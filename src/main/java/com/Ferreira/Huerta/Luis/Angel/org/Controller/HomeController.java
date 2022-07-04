dbfcdbcde4701caea562ed850da5302342a0c99a
luis <luis_michel9509@hotmail.com>
2022-07-03 21:54:51 -0500

    firs commi

---
+++ src/main/java/com/Ferreira/Huerta/Luis/Angel/org/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.Ferreira.Huerta.Luis.Angel.org.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/home")
+	public  String mostrarHome() {
+		return "home";
+	}
+}
